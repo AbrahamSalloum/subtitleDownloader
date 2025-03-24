45b19ad34744c0b98e6a46868b380ad635ba3e0f
Abraham Salloum <abrahamsalloum@gmail.com>
2025-03-25 10:19:19 +1100

    column sorting

--- subsl/MainWindow.xaml.cs
+++ subsl/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 using System.ComponentModel;
+using System.Windows.Data;
 
 
 namespace subsl
@@ -26,6 +27,9 @@ namespace subsl
         private ItemList CurrentSelected;
         private OpenSubtitlesAPI subs;
         private bool LoggedIn = false;
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
 
         public MainWindow()
         {
@@ -80,14 +84,7 @@ namespace subsl
 
             StatusTxt = null;
         }
-        private void ListViewItem_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            var listView = sender as ListView;
-            CurrentSelected = listView?.SelectedItem as ItemList;
-            Debug.WriteLine(CurrentSelected?.attributes?.url);
-            MessageBox.Show("Nothing.");
 
-        }
         private void ListViewItem_MouseClick(object sender, System.Windows.Input.MouseButtonEventArgs? e)
         {
             var listView = sender as ListView;
@@ -199,8 +196,71 @@ namespace subsl
             }
         }
 
+        void GridViewColumnHeaderClickedHandler(object sender,
+                                                RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+
+                    Sort(sortBy, direction);
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowUp"] as DataTemplate;
+                    }
+                    else
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowDown"] as DataTemplate;
+                    }
+
+                    // Remove arrow from previously sorted header
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.HeaderTemplate = null;
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
 
 
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(sublist.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
+        }
         public event PropertyChangedEventHandler? PropertyChanged;
 
 
