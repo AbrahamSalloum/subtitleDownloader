86d41363280d53c78793a8a2d45407b2a1dab87c
Abraham Salloum <abrahamsalloum@gmail.com>
2025-05-17 17:09:23 +1000

    cleanup

--- subsl/App.xaml.cs
+++ subsl/App.xaml.cs
@@ -8,9 +8,7 @@ using System.Text.RegularExpressions;
 
 namespace subsl
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
